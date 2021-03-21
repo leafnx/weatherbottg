5117a109a5bf8b856a252f001cc86e3bec71a026
MSST1 <nikita1galich@yandex.ru>
2021-03-21 20:21:28 +0300

    hack commit

--- index.js
+++ index.js
@@ -24,26 +24,8 @@ const bot = new Telegraf('1717568476:AAHBD98TBElaJ7YQ4aTFZlK9OBRpzxBi-JU')
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('location', (ctx) => {
-  options.params.lat = ctx.message.location.latitude;
-  options.params.lon = ctx.message.location.longitude;
-
-
-  let a = function (response) {
-    let b = response.data;
-  	ctx.reply(`Current weather in your location:
-      ${b.weather[0].main} (${b.weather[0].description})
-      Temperature: ${b.main.temp}°C, feels like ${b.main.feels_like}°C
-      Wind: ${b.wind.speed} mps`);
-    console.log(response.data);
-  }
-
-
-  axios.request(options).then(a).catch(function (error) {
-  	console.error(error);
-  });
-
-    console.log(options);
-})
+  ctx.reply(`Сасиии`)
+};
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()
 
